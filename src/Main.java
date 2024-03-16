4dc6947792ff59155c6a397a2aeee395e1d92af7
Volodymyr Kuryliak <volodymyr.kuryliakk@gmail.com>
2024-03-16 01:14:50 +0100

    created console inputs for data of user

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
 
         int firstNumber, secondNumber, result;
         String answer = "Answer is: ";
-        char operation;
+        String operation;
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        firstNumber = input.nextInt();
+        System.out.print("Enter operation: ");
+        operation = input.next();
+        System.out.print("Enter second number: ");
+        secondNumber = input.nextInt();
+
     }
 }
